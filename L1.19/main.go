7efbe48a27540ca2474319396ee56734fd128cc4
Alexander <sarumo0@yandex.ru>
2025-09-05 15:16:34 +0300

    Done

---
+++ L1.19/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input string
+	_, err := fmt.Scan(&input)
+
+	if err != nil {
+		fmt.Println("Ошибка ввода:", err)
+	}
+
+	length := len(input)
+	result := make([]rune, length)
+
+	for i, r := range []rune(input) {
+		result[(length-1)-i] = r
+	}
+
+	fmt.Println(string(result))
+}
